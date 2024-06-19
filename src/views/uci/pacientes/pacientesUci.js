4633b2c280727c111d72adabf33fe8ce8bbda87d
Web HM <web@hmetro.med.ec>
2024-06-19 10:26:26 -0500

    dev.ajustes.uci
    dev ajustes se corrige bug de cuidado sgenerales

--- src/views/uci/pacientes/pacientesUci.js
+++ src/views/uci/pacientes/pacientesUci.js
@@ -4571,7 +4571,7 @@ class PacientesUCI extends App {
         options.map((option) => {
             FecthUci.dataSecciones.filter((obj) => {
                 let _obj = JSON.parse(obj.DATASECCION);
-                if (_obj.id === option.id) {
+                if (_obj.id === option.id && _obj.seccion == 'CuidadosGenerales') {
                     res.push(_obj);
                 }
             });
