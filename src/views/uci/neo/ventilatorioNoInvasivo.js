d18112dba9ea96969851fd9c67d27d27298e78f3
Web HM <web@hmetro.med.ec>
2024-07-31 15:53:39 -0500

    dev.hm.neo

--- src/views/uci/neo/ventilatorioNoInvasivo.js
+++ src/views/uci/neo/ventilatorioNoInvasivo.js
@@ -223,7 +223,7 @@ class VentilatorioNoInvasivo {
                     mRender: function(data, type, full) {
                         return (full.valor != null ? full.valor : '<div class="text-center pd-l-0 pd-r-0"><hr style="border-color:#001737;"/></div>');
                     },
-                    visible: false,
+                    visible: true,
                     aTargets: [6],
                     orderable: true,
 
@@ -232,7 +232,7 @@ class VentilatorioNoInvasivo {
                     mRender: function(data, type, full) {
                         return (full.cantidad != null ? full.cantidad : '<div class="text-center pd-l-0 pd-r-0"><hr style="border-color:#001737;"/></div>');
                     },
-                    visible: true,
+                    visible: false,
                     aTargets: [7],
                     orderable: true,
 
